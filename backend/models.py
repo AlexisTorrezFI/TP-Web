5e565df02240cf94f46c7ca4cc311ded05ee9ef8
Alexis Torrez <atorrez@fi.uba.ar>
2024-07-03 17:48:34 -0300

    añado archivo models.py a la carpeta backend con las tablas : productos,categorias,comentarios

---
+++ backend/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Producto(db.Model):
+    __tablename__='productos'
+    id = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Integer , nullable=False)
+    cantidad = db.Column(db.Integer , default=1)
+    image = db.Column(db.String(255))
+    #me_gusta = db.Column(db.Integer , default=0)
+    categoria_id =db.Column(db.Integer,db.ForeignKey('categorias.id'))
+    comentarios=db.relationship("Comentario")
+
+class Categoria(db.Model):
+    __tablename__='categorias'
+    id = db.Column(db.Integer,primary_key=True)
+    categoria = db.Column(db.String(100), nullable=False)
+
+class Comentario(db.Model):
+    __tablename__='comentarios'
+    id = db.Column(db.Integer,primary_key=True)
+    producto_id=db.Column(db.Integer, db.ForeignKey('productos.id'))
+    comentario = db.Column(db.String(255))
